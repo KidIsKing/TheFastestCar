73442057611233dcfe8d837b5c30494b2619345b
KidIsKing <anulamasalova@gmail.com>
2026-05-21 18:54:39 +0500

    add default_pygame_settings

---
+++ default_pygame_settings.py
@@ -0,0 +1,26 @@
+import pygame
+
+
+pygame.init()
+
+WIGHT, HEIGHT = 1280, 720
+
+display = pygame.display.set_mode((WIGHT, HEIGHT))
+pygame.display.set_caption("TheFastestCar")
+clock = pygame.time.Clock()
+
+running = True
+
+while running:
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            running = False
+
+    if event.type == pygame.KEYDOWN:
+        if event.type == pygame.K_ESCAPE:
+            running = False
+
+    pygame.display.flip()
+    clock.tick(60)
+
+pygame.quit()

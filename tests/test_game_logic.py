2f452bd185b817e2bc8d5e6f95777e634f2eb0ff
KidIsKing <anulamasalova@gmail.com>
2026-06-06 22:35:04 +0500

    delete some comments

--- tests/test_game_logic.py
+++ tests/test_game_logic.py
@@ -4,7 +4,6 @@ import pytest
 import sys
 from pathlib import Path
 
-# Добавляем корень проекта (если conftest.py не подхватился)
 project_root = Path(__file__).parent.parent
 sys.path.insert(0, str(project_root))
 
